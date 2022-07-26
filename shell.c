ee5ba85cafd9e2c88665aceab61f74922ddde959
Jose Alvarez <5135@holbertonschool.com>
2022-07-26 07:07:15 -0700

    shell

--- shell.c
+++ shell.c
@@ -3,23 +3,30 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <sys/wait.h>
-int main(void)
+int main(int argc, char **argv)
 {
-(void) argc;
+	(void) argc;
+	char *str = 0;
+	size_t n = 0;
 	int status;
-
-	if (fork() == 0)
+	char *token;
+	char *delim = " \t\n";
+	char *alambre[] = {"", NULL};
+	printf("$");
+	while (getline (&str, &n, stdin) != -1)
 	{
-		printf("From child\n");
-		if (execve(argv[0], argv, NULL) == -1)
+		token = strtok(str, delim);
+		printf("your lines is: %s", token);
+		if (execve(token, alambre, NULL) == -1)
 		{
 			perror("Error:");
 		}
+		else
+		{
+			wait(&status);
+			printf("end of child\n");
+		}
 	}
-	else
-	{
-		wait(&status);
-		printf("end of child\n");
-	}
+	free(str);
 	return (0);
 }
